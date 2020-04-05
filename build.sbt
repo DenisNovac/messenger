e642dc33a8d96c13f0466b47988ae470c210a0bb
DenisNovac <ponydnk@gmail.com>
2020-04-05 12:19:28 +0500

    Generic monad logic for tapir routes

--- build.sbt
+++ build.sbt
@@ -15,18 +15,26 @@ libraryDependencies += "org.log4s" %% "log4s" % "1.8.2"
 libraryDependencies += "com.softwaremill.sttp.tapir" %% "tapir-core" % "0.13.0"
 libraryDependencies += "com.softwaremill.sttp.tapir" %% "tapir-http4s-server" % "0.13.0"
 
+val circeVersion = "0.13.0"
+
+libraryDependencies ++= Seq(
+  "io.circe" %% "circe-core",
+  "io.circe" %% "circe-generic",
+  "io.circe" %% "circe-parser"
+).map(_ % circeVersion)
+
 
 val http4sVersion = "0.21.3"
+
 libraryDependencies ++= Seq(
   "org.http4s" %% "http4s-core" % http4sVersion,
   "org.http4s" %% "http4s-server" % http4sVersion,
   "org.http4s" %% "http4s-blaze-server" % http4sVersion,
-  "org.http4s" %% "http4s-dsl" % http4sVersion,
-  //"org.http4s" %% "http4s-blaze-client" % http4sVersion
-  //"org.http4s" %% "http4s-circe" % http4sVersion
+  "org.http4s" %% "http4s-dsl" % http4sVersion
 )
 
 val catsVersion = "2.1.1"
+
 libraryDependencies ++= Seq(
    "org.typelevel" %% "cats-core" % catsVersion,
    "org.typelevel" %% "cats-effect" % catsVersion
--- src/main/scala/app/Server.scala
+++ src/main/scala/app/Server.scala
@@ -1,57 +1,78 @@
 package app
 
-import cats.effect.ExitCode
+
 import sttp.model.StatusCode
 import sttp.tapir._
 import sttp.tapir.server.ServerDefaults.StatusCodes
 
-/** Секция построения реализации */
-import scala.concurrent.ExecutionContext
+/** Generic logic */
+import cats.Monad  // for generic logic on pure syntax
+import cats.syntax.either._ // for asRight
+import cats.syntax.applicative._  // for pure
 
-import cats.syntax.either._ // для asRight
-import cats.syntax.functor._ // для as
-import cats.syntax.semigroupk._  // для <+>
-import cats.effect.IO
-import cats.effect.ContextShift
-import cats.effect.Timer
+
+/** For http4s server */
+import scala.concurrent.ExecutionContext
+import cats.syntax.functor._ // for as
+import cats.syntax.semigroupk._ // for <+>
+import cats.effect.{IO, ContextShift, Timer, ExitCode}
 
 import sttp.tapir.server.http4s._
 import org.http4s.server.blaze.BlazeServerBuilder
-import org.http4s.syntax.all._ // для orNotFound
+import org.http4s.syntax.all._ // for orNotFound
 import org.http4s.HttpRoutes
 import org.http4s.server.Router
 
-/** Модель данных */
+
 sealed trait Data
 final case class Message(timestamp: Int, user: String, text: String)
     extends Data
 final case class Sync(timestamp: Int) extends Data
 
+/** Routes separately from logic */
 object Routes {
 
   val health: Endpoint[Unit, Unit, StatusCode, Nothing] =
     endpoint.get
-      .in("health")
-      .out(statusCode)
+      .in("health") // Path
+      .out(statusCode) // Output type
 
   // GET /hello?name=...
   val hello: Endpoint[String, Unit, String, Nothing] =
     endpoint.get
       .in("hello")
-      .in(query[String]("name"))
+      .in(query[String]("name")) // Parameter
       .out(stringBody)
 
 }
 
-object Server extends App {
+/**
+  * Logic is separate from routes definitions
+  * It may be IO (http4s) or Future (Akka Http)
+  */
+class Logic[F[_]: Monad] {
+
+  def health: F[Either[Unit, StatusCode]] =
+    StatusCodes.success
+      .asRight[Unit]
+      .pure[F]
+
+  def hello(name: String): F[Either[Unit, String]] =
+    s"Hello, $name".asRight[Unit].pure[F]
+}
+
+object Http4sServer extends App {
 
   implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.global
   implicit val contextShift: ContextShift[IO] = IO.contextShift(ec)
   implicit val timer: Timer[IO] = IO.timer(ec)
 
-  /** Преобразование роутов Tapir в Http4s */
-  val health: HttpRoutes[IO] = Routes.health.toRoutes(_ => IO(StatusCodes.success.asRight[Unit]))
-  val hello: HttpRoutes[IO] = Routes.hello.toRoutes(name => IO(s"Hello, $name".asRight[Unit]))
+  // import cats.instances.future._ if one want to get Logic from Future
+  val logic = new Logic[IO]
+
+  /** Routes Tapir to Http4s */
+  val health: HttpRoutes[IO] = Routes.health.toRoutes(_ => logic.health)
+  val hello: HttpRoutes[IO] = Routes.hello.toRoutes(name => logic.hello(name))
   val concat = health <+> hello
 
   val routes = Router("/" -> concat).orNotFound
