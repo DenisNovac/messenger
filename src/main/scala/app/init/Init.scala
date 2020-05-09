b90bbc374f2c9bb99c237e6d9df4d7725801c63e
DenisNovac <ponydnk@gmail.com>
2020-05-09 19:56:22 +0500

    PostgreSQL integration:
    - Added facility to create tables in postgresql (with timeout and retries),
    - Added postgresql to docker-compose;
    - Added config section for postgresql

--- src/main/scala/app/init/Init.scala
+++ src/main/scala/app/init/Init.scala
@@ -1,17 +1,23 @@
-package app
+package app.init
 
 import app.model.ServerConfig
 import pureconfig.ConfigSource
-import pureconfig.ConfigSource
 import pureconfig.generic.auto._
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.ExecutionContext.global
 
 /**
   * An object to contain application config for shared access
   */
-object ServerConfigReader {
+object Init {
 
   val config: ServerConfig = ConfigSource.file("./application.conf").load[ServerConfig] match {
     case Left(value)  => throw new Error(value.toString)
     case Right(value) => value
   }
+
+  implicit val ec: ExecutionContext = global
+  val postgres                      = new PostgresSession(config.db)
+
 }
