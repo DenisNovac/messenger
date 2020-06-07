54c32a2f4f702d325a0af34376fff414b8b8ceaa
DenisNovac <ponydnk@gmail.com>
2020-06-07 14:51:47 +0500

    scalafmt fix

--- src/main/scala/app/model/quillmappings/QuillInstantMapping.scala
+++ src/main/scala/app/model/quillmappings/QuillInstantMapping.scala
@@ -9,4 +9,3 @@ object QuillInstantMapping {
   implicit val instantEncoding: MappedEncoding[Instant, Date] = MappedEncoding[Instant, Date](Date.from)
   implicit val instantDecoding: MappedEncoding[Date, Instant] = MappedEncoding[Date, Instant](_.toInstant())
 }
-
